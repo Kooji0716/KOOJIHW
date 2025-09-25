39947169bc4d4ced67c013cc467e80650fdd5460
Kooji0716 <930716jim@GMAIL.COM>
2025-09-25 15:04:17 +0800

    新增document.write 取代body的內容

---
+++ HW4_JavaScript1/static/111302019_js_2.js
@@ -0,0 +1,8 @@
+document.write("<div style='max-width:500px;margin:20px auto;font-family:Arial;'>");
+
+// 標題
+document.write("<h1>尻哭累特</h1>");
+
+// 輸入框
+document.write("<input type='text' id='screen' style='width:100%;font-size:20px;margin-bottom:10px;'><br>");
+
