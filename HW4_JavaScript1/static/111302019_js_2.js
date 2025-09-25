a8e5592a4df25dfe2e790e12678ef42eb43b8fb5
Kooji0716 <930716jim@GMAIL.COM>
2025-09-25 15:07:39 +0800

    新增清除的clear的涵式，還有把按按鈕的數值加進輸入框裡的涵式

--- HW4_JavaScript1/static/111302019_js_2.js
+++ HW4_JavaScript1/static/111302019_js_2.js
@@ -6,3 +6,13 @@ document.write("<h1>尻哭累特</h1>");
 // 輸入框
 document.write("<input type='text' id='screen' style='width:100%;font-size:20px;margin-bottom:10px;'><br>");
 
+
+// appendToScreen(txt)：把按下的按鈕文字加進輸入框
+function appendToScreen(txt){
+    document.getElementById("screen").value += txt;
+}
+
+// clearScreen()：clear的涵式，清除內容
+function clearScreen(){
+    document.getElementById("screen").value = "";
+}
